20f6937af5d23b9f081066105cc10f7f5dad0299
plammo <plammo@live.com>
2014-08-19 19:26:11 +1200

    latest changes and updates

---
+++ code/includes/head.php
@@ -0,0 +1,150 @@
+<head>
+	<meta charset="UTF-8">
+	<title>Homepage</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	<link href='http://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Share+Tech' rel='stylesheet' type='text/css'>
+	
+	
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="row row_1 clearfix">
+			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+			</div>
+			<div class="col-xs-6 col-sm-6 logo col-md-6 col-lg-6">
+			<img src="images/skie_logo.png" alt="pic" title="picture of skie logo" class="img-responsive logo_pic">
+			</div>
+			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+			</div>
+		</div>
+
+		<div class="row row_2 clearfix">
+			<div class="col-sm-12 col-md-12 col-lg-12">
+			<img src="images/skier.png" alt="pic" title="skiers background" class="img-responsive">
+			</div>
+		</div>
+
+		<div class="row row_3 clearfix">
+			<div class="col-sm-5 col-md-5 col-lg-5">
+			</div>
+			<div class="col-sm-5 col-md-5 col-lg-5">
+			<h2>How long did it take me?</h2>
+			<h2>Was my posture right?</h2>
+			<h2 class="speed">What was my maximum speed?</h2>
+			</div>
+			<div class="col-sm-2 col-md-2 col-lg-2">
+			</div>
+		</div>
+
+		<div class="row row_4 clearfix">
+			<div class="col-sm-4 col-md-4 col-lg-4">
+			</div>
+			<div class="col-sm-4 col-md-4 col-lg-4">
+			<h1>What if...</h1>
+			</div>
+			<div class="col-sm-4 col-md-4 col-lg-4">
+			</div>
+		</div>
+
+		<div class="row row_5 clearfix">
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+			<div class="col-sm-6 col-md-6 col-lg-6">
+			<h3>something answers all<br>your questions?</h3>
+			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+			
+		</div>
+
+		<div class="row row_6 clearfix">
+			<div class="col-sm-4 col-md-4 col-lg-4">
+			</div>
+			<div class="col-sm-4 col-md-4 col-lg-4">
+			<h1>What if...</h1>
+			</div>
+			<div class="col-sm-4 col-md-4 col-lg-4">
+			</div>
+		</div>
+
+		<div class="row row_7 clearfix">
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+			<div class="col-sm-6 logo col-md-6 col-lg-6">
+			<img src="images/skie_logo.png" alt="pic" title="picture of skie logo" class="img-responsive logo_pic">
+			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+		</div>
+
+		<div class="row row_8 clearfix">
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+			<div class="col-sm-3 logo col-md-3 col-lg-3">
+			<img src="images/insole.png" alt="pic" title="picture of insole" class="img-responsive logo_pic">
+			</div>
+			<div class="col-sm-3 logo col-md-3 col-lg-3">
+			<img src="images/ski_boot.png" alt="pic" title="picture of ski boot" class="img-responsive logo_pic">
+			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+		</div>
+
+		
+		<div class="row row_9 clearfix">
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+			<div class="col-sm-6 col-md-6 col-lg-6">
+			<h3>just a simple insole</h3>
+			<p>Contrary to popular belief, Lorem Ipsum is not simply random text.
+			It has it's roots in a piece of classical Latin literature from 45 BC,
+			making it over 2000 years old.
+			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+		</div>
+
+		<div class="row row_10 clearfix">
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+			<div class="col-sm-3 logo col-md-3 col-lg-3">
+			<img src="images/rss_feed.png" alt="pic" title="picture of insole" class="img-responsive logo_pic">
+			</div>
+			<div class="col-sm-3 logo col-md-3 col-lg-3">
+			<img src="images/smart_phone.png" alt="pic" title="picture of ski boot" class="img-responsive logo_pic">
+			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3">
+			</div>
+		</div>
+
+		<div class="row row_11 clearfix">
+			<div class="col-sm-2 col-md-2 col-lg-2">
+			</div>
+			<div class="col-sm-8 col-md-8 col-lg-8">
+			<h3>connect to your smartphone</h3>
+			<p>Contrary to popular belief, Lorem Ipsum is not simply random text.
+			It has it's roots in a piece of classical Latin literature from 45 BC,
+			making it over 2000 years old.
+			</div>
+			<div class="col-sm-2 col-md-2 col-lg-2">
+			</div>
+		</div>
+
+
+		<div class="row row_12 clearfix">
+			<div class="col-sm-12 col-md-12 col-lg-12">
+			<img src="images/mountain_foot.png" alt="pic" title="mountain range image" class="img-responsive">
+			</div>
+
+		</div>
+		</body>
+
+
+	
---
+++ code/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php
+
+require_once('includes/head.php');
+//require_once('includes/main_content.php');
+//require_once('includes/navmenu.php');
+//require_once('includes/footer.php');
+
+?>
+
+
